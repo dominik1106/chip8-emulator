633fbe957980bf2eefc50ad48bf82f613d3082bd
Dominik Schneider <dominik.p.s@outlook.de>
2023-06-25 03:15:52 +0200

    create basic window using SDL2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cstdint>
 #include <stack>
 #include <fstream>
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
 uint8_t memory[4096];
 int program_counter;
@@ -28,9 +30,41 @@ int read_file_into_memory(std::string path, uint8_t* mem) {
 int main() {
     int size = read_file_into_memory("IBMLogo.ch8", memory);
 
-    if(size > -1) {
-        for(int i = 0; i < size; i++) {
-            std::cout << +memory[512 + i] << ',';
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    } else {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 64, 32, SDL_WINDOW_SHOWN );
+        
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        } else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
         }
     }
+
+    
+
+    //Destroy window
+    SDL_DestroyWindow( window );
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
 }
